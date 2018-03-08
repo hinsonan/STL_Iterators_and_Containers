9a29cf0cf4af633d1a973f5a62c4a65ed11abacd
Andrew Hinson <hinsonan@gmail.com>
2018-03-08 13:15:14 -0600

    initial commit

---
+++ QuickSelect/QuickSelect/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QuickSelect
+//
+//  Created by Andrew Hinson on 3/8/18.
+//  Copyright © 2018 Andrew Hinson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
