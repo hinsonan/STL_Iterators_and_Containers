b74008aa13bf3391d7a5fc790527c811492b49b8
Andrew Hinson <hinsonan@gmail.com>
2018-03-15 17:35:19 -0500

    sorts random vec ints

--- QuickSelect/QuickSelect/main.cpp
+++ QuickSelect/QuickSelect/main.cpp
@@ -18,20 +18,20 @@ using namespace std;
 
 
 template <typename Iterator>
-Iterator selectTest(Iterator begin, Iterator end, int n) {
+Iterator quickSelectSort(Iterator begin, Iterator end, int k) {
     typedef typename iterator_traits<Iterator>::value_type T;
     while (true) {
-        Iterator pivotIt = begin + rand() % distance(begin, end);
-        iter_swap(pivotIt, end-1);  // Move pivot to end
-        pivotIt = partition(begin, end-1, bind2nd(less<T>(), *(end-1)));
-        iter_swap(end-1, pivotIt);  // Move pivot to its final place
-        if (n == pivotIt - begin) {
-            return pivotIt;
-        } else if (n < pivotIt - begin) {
-            end = pivotIt;
+        Iterator pivot = begin + rand() % distance(begin, end); //chooses random pivot value
+        iter_swap(pivot, end-1);  // Move pivot to end
+        pivot = partition(begin, end-1, bind2nd(less<T>(), *(end-1))); //uses the partition func from the STL
+        iter_swap(end-1, pivot);  // Move pivot to its final place
+        if (k == pivot - begin) {
+            return pivot;
+        } else if (k < pivot - begin) {
+            end = pivot;
         } else {
-            n -= pivotIt+1 - begin;
-            begin = pivotIt+1;
+            k -= pivot+1 - begin;
+            begin = pivot+1;
         }
     }
 }
@@ -39,25 +39,20 @@ Iterator selectTest(Iterator begin, Iterator end, int n) {
 int main() {
     vector<int> intVec;
     
-    vector<int>::iterator vectorIterator;
-    //Insert elements
-    intVec.push_back(10);
-    intVec.push_back(34);
-    intVec.push_back(65);
-    intVec.push_back(3);
-    intVec.push_back(55);
-    intVec.push_back(80);
-    intVec.push_back(90);
-    intVec.push_back(7);
-    intVec.push_back(79);
-    intVec.push_back(23);
+    /* initialize random seed: */
+    srand (time(NULL));
+    
+    //Insert elements into vector
+    for(int k = 0; k < 10; k++){
+        intVec.push_back(rand() % 100);
+    }
 
     
     
     
     for (int i = 0; i < 10; i++) {
         
-        selectTest(intVec.begin(), intVec.end(), i);
+        quickSelectSort(intVec.begin(), intVec.end(), i);
         
     }
     
